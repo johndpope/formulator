36b6eda3cf203675b41b65b0f918c00dc4661d8c
skylerknight <31410434+skylerknight@users.noreply.github.com>
2021-08-22 22:17:24 -0500

    Updated formulator provider state to reducer

--- providers/FormulatorProvider.tsx
+++ providers/FormulatorProvider.tsx
@@ -1,46 +1,64 @@
-import React from 'react'
+import React from "react";
 
+// FORMULA CONTEXT & USAGE HOOK
 const FormulatorContext = React.createContext<Partial<FormulatorContextProps>>({});
 export const useFormulatorContext = () => React.useContext(FormulatorContext);
 
-export default function FormulatorProvider({data, children} : FormulatorProviderProps) {
+// FORMULA REDUCER
+const formulaReducer = (state: Formula, action: FormulaAction) => {
+	const { type, payload } = action;
+	if (payload == null) return state;
 
-   const [formula, setFormula] = React.useState<Formula>({
-      name: 'New Formula',
-      equation: '',
-      result: null,
-      openBrackets: 0,
-      lastConstantType: '',
-      variables: []
-   });
+	switch (type) {
+		case "CHANGE_NAME":
+			return { ...state, name: payload.toString() };
+		default:
+			return state;
+	}
+};
 
-   return (
-      <FormulatorContext.Provider value={{formula, setFormula}}>
-         {children}
-      </FormulatorContext.Provider>
-   )
+// FORMULA CONTEXT PROVIDER
+export default function FormulatorProvider({ data, children }: FormulatorProviderProps) {
+	const [formula, dispatch] = React.useReducer(formulaReducer, {
+		name: "New Formula",
+		equation: "",
+		result: null,
+		openBrackets: 0,
+		lastConstantType: "",
+		variables: [],
+	});
+
+	return (
+		<FormulatorContext.Provider value={{ formula, dispatch }}>
+			{children}
+		</FormulatorContext.Provider>
+	);
 }
-
 
 type Variable = {
-   [key: string] : string,
-}
+	[key: string]: string;
+};
 
 type Formula = {
-   name: string,
-   equation: string,
-   result: string|null,
-   openBrackets: number
-   lastConstantType: string,
-   variables: Array<Variable>
-}
+	name: string;
+	equation: string;
+	result: string | null;
+	openBrackets: number;
+	lastConstantType: string;
+	variables: Array<Variable>;
+};
+
+type FormulaAction = {
+	type: string;
+	payload: string | number;
+};
 
 interface FormulatorProviderProps {
-   data?: Formula
-   children: React.ReactNode|Array<React.ReactNode>
+	data?: Formula;
+	children: React.ReactNode | Array<React.ReactNode>;
 }
 
 interface FormulatorContextProps {
-   formula: Formula,
-   setFormula: React.Dispatch<React.SetStateAction<Formula>>
+	formula: Formula;
+	dispatch: React.Dispatch<FormulaAction>;
 }
--- screens/FormulatorScreen.tsx
+++ screens/FormulatorScreen.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import tw from "../styles/tailwind";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { useFormulatorContext } from "../providers/FormulatorProvider";
 
 export default function FormulatorScreen() {
-	const { formula, setFormula } = useFormulatorContext();
+	const { formula, dispatch } = useFormulatorContext();
 
 	return (
 		<View style={tw`flex-1 items-center justify-center`}>
