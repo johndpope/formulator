67e141beaf200e28b8016fab2644d0b2ee91f74e
skylerknight <31410434+skylerknight@users.noreply.github.com>
2021-09-02 15:47:43 -0500

    Added palenight theme to Themes.tsx

--- styles/Themes.tsx
+++ styles/Themes.tsx
@@ -28,3 +28,32 @@ export const darker: Theme = {
 		error: "#ff5370",
 	},
 };
+
+export const palenight: Theme = {
+	brand: "#F78C6C",
+	border: "#2b2a3e",
+	text: {
+		primary: "#FFFFFF",
+		secondary: "#676E95",
+	},
+	background: {
+		primary: "#202331",
+		secondary: "#292D3E",
+	},
+	button: {
+		primary: "#F78C6C",
+		secondary: "#303348",
+	},
+	colors: {
+		green: "#c3e88d",
+		yellow: "#ffcb6b",
+		blue: "#82aaff",
+		red: "#f07178",
+		purple: "#c792ea",
+		orange: "#f78c6c",
+		cyan: "#89ddff",
+		gray: "#616161",
+		contrast: "#eeffff",
+		error: "#ff5370",
+	},
+};
