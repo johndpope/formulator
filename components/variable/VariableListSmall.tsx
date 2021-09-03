2d78cd0137bf398ec39c92876d895bf865606498
skylerknight <31410434+skylerknight@users.noreply.github.com>
2021-09-03 12:00:09 -0500

    Added placeholder for empty variables

--- components/variable/VariableListSmall.tsx
+++ components/variable/VariableListSmall.tsx
@@ -9,6 +9,7 @@ import { useThemeContext } from "../../providers/ThemeProvider";
 
 import { Formula, FormulaAction } from "../../types/FormulatorTypes";
 import { theme } from "../../tailwind.config";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 interface VariableListSmallProps {
 	formula: Formula;
@@ -24,46 +25,57 @@ export function VariableListSmall({ formula, dispatch }: VariableListSmallProps)
 	};
 
 	return (
-		<FlatList
-			horizontal
-			data={formula.variables}
-			keyExtractor={(item) => `variable-chip-${item.name}`}
-			contentContainerStyle={tw`flex flex-row items-center`}
-			renderItem={({ item }) => (
-				<Pressable
-					onPress={() =>
-						dispatch({
-							type: "INSERT_CONSTANT",
-							payload: { constantType: "EQ_VARIABLE", constantValue: `${item.name}` },
-						})
-					}
-					onLongPress={() => navigation.navigate("Variable", { variable: item })}
-					style={[
-						{ borderColor: theme.colors[item.color] },
-						tw`flex flex-row px-2 py-1.5 mr-3 rounded-md overflow-hidden border`,
-					]}>
-					<View
-						style={[
-							{ backgroundColor: theme.colors[item.color] },
-							tw`absolute inset-y-0 inset-x-0 opacity-10`,
-						]}></View>
-					<Text
-						style={[
-							{ color: theme.colors[item.color] },
-							variableTextStyle,
-							tw`text-sm pr-1`,
-						]}>{`{`}</Text>
-					<Text style={[{ color: theme.text.primary }, variableTextStyle, tw`text-sm capitalize`]}>
-						{item.name}
+		<>
+			{formula.variables.length ? (
+				<FlatList
+					horizontal
+					data={formula.variables}
+					keyExtractor={(item) => `variable-chip-${item.name}`}
+					contentContainerStyle={tw`flex flex-row items-center`}
+					renderItem={({ item }) => (
+						<Pressable
+							onPress={() =>
+								dispatch({
+									type: "INSERT_CONSTANT",
+									payload: { constantType: "EQ_VARIABLE", constantValue: `${item.name}` },
+								})
+							}
+							onLongPress={() => navigation.navigate("Variable", { variable: item })}
+							style={[
+								{ borderColor: theme.colors[item.color] },
+								tw`flex flex-row px-2 py-1.5 mr-3 rounded-md overflow-hidden border`,
+							]}>
+							<View
+								style={[
+									{ backgroundColor: theme.colors[item.color] },
+									tw`absolute inset-y-0 inset-x-0 opacity-10`,
+								]}></View>
+							<Text
+								style={[
+									{ color: theme.colors[item.color] },
+									variableTextStyle,
+									tw`text-sm pr-1`,
+								]}>{`{`}</Text>
+							<Text style={[{ color: theme.text.primary }, variableTextStyle, tw`text-sm capitalize`]}>
+								{item.name}
+							</Text>
+							<Text
+								style={[
+									{ color: theme.colors[item.color] },
+									variableTextStyle,
+									tw`text-sm pl-1`,
+								]}>{`}`}</Text>
+						</Pressable>
+					)}
+				/>
+			) : (
+				<View style={tw`flex-1 flex flex-row items-center justify-center`}>
+					<Text style={[{ color: theme.text.secondary, fontFamily: "Poppins_400Regular" }, tw`mr-2`]}>
+						Click "New" to add a new variable
 					</Text>
-					<Text
-						style={[
-							{ color: theme.colors[item.color] },
-							variableTextStyle,
-							tw`text-sm pl-1`,
-						]}>{`}`}</Text>
-				</Pressable>
+					<FontAwesomeIcon icon={["fal", "arrow-right"]} size={12} color={theme.text.secondary} />
+				</View>
 			)}
-		/>
+		</>
 	);
 }
